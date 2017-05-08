bde49a5f83b5486d7ce44d302d75b057ab0c9dd3
Janós Potecki <jpotecki@users.noreply.github.com>
2017-05-08 12:12:47 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,14 +4,16 @@
 sudo apt-get update
 
 sudo apt-get -y install emacs git
+git config --global user.name "jpotecki"
 
-# install haskell stack toop 
+# install haskell stack 
 
 curl -sSL https://get.haskellstack.org/ | sh
 stack setup
 stack install hscolor
 stack install quickcheck
 stack install infero
+
 # install bash and make default shell
 apt-get install -y fish
 chsh -s /usr/bin/fish
