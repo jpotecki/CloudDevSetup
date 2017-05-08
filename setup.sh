424373debcbe3153b61cfad5b063b275ef025e5b
Janós Potecki <jpotecki@users.noreply.github.com>
2017-05-08 11:28:37 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,7 +15,4 @@ stack install infero
 # install bash and make default shell
 apt-get install -y fish
 chsh -s /usr/bin/fish
-
-# config file
-mkdir -p ~/.config/fish
-set -g -x fish_greeting 'I am the sword in the darkness. I am the watcher on the walls. I am the fire that burns against the cold, the light that brings the dawn, the horn that wakes the sleepers, the shield that guards the realms of men'
+fish
