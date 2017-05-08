dcdcd0fd5facd5ec31df4133a6b3d477d32c99fd
Janós Potecki <jpotecki@users.noreply.github.com>
2017-05-08 11:19:33 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 # basics
 sudo apt-get update
 
-sudo apt-get install emacs git
+sudo apt-get -y install emacs git
 
 # install haskell stack toop 
 
@@ -13,11 +13,9 @@ stack install hscolor
 stack install quickcheck
 stack install infero
 # install bash and make default shell
-apt-get install fish
+apt-get install -y fish
 chsh -s /usr/bin/fish
 
 # config file
 mkdir -p ~/.config/fish
 set -g -x fish_greeting 'I am the sword in the darkness. I am the watcher on the walls. I am the fire that burns against the cold, the light that brings the dawn, the horn that wakes the sleepers, the shield that guards the realms of men'
-
-
